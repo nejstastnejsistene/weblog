2afc4c579f9d085eae106aee308131f6afc6729d
Peter Johnson <pajohnson@email.wm.edu>
2013-08-30 17:04:46 -0400

    Added a better text->bf algorithm that uses loops instead of just plusses and minuses.

--- bf.py
+++ bf.py
@@ -1,7 +1,8 @@
 import StringIO
 
-def tostring_naive(s):
-    f = StringIO.StringIO()
+
+def tostring_v1(s, f):
+    '''A naive algorithm that just prints +'s and -'s within a single cell.'''
     n = 0
     for b in map(ord, s):
         if n < b:
@@ -10,8 +11,47 @@ def tostring_naive(s):
             f.write('-'*(n-b))
         f.write('.')
         n = b
-    return f.getvalue()
 
-def tostring(s, func=tostring_naive):
-    bf = func(s)
-    return '\n'.join(bf[i:i+80] for i in range(0, len(bf), 80))
+def findfactors(n):
+    '''Yields [a, b, c] triplets where a * b + c == n'''
+    for m in range(n, int(n**0.5), -1):
+        for k in range(int(m**0.5), m):
+            if m % k == 0:
+                assert k * (m / k) + n - m == n
+                yield [k, m / k, n - m]
+
+def factor(n):
+    '''Find the smallest set of factors.'''
+    return min(findfactors(n), key=sum)
+
+
+def tostring_v2(s, f):
+    '''A slightly smarter algorithm that uses loops to optimize long strings
+       of +'s or -'s. The second cell is used as an accumulator.
+    '''
+    n = 0
+    for b in map(ord, s):
+        ch = '+' if n < b else '-'
+        diff = abs(b - n)
+        if diff < 8:
+            # If the difference is small, just write the +/-s.
+            f.write(ch*diff)
+        else:
+            # Otherwise factor the number and write a loop.
+            x, y, z = factor(abs(b - n))
+            f.write('>')
+            f.write('+'*x)
+            f.write('[<')
+            f.write(ch*y)
+            f.write('>-]<')
+            f.write(ch*z)
+        f.write('.')
+        n = b
+
+
+def tostring(s, func=tostring_v2, linewidth=80):
+    f = StringIO.StringIO()
+    func(s, f)
+    bf = f.getvalue()
+    # Split into evenly sized lines.
+    return '\n'.join(bf[i:i+linewidth] for i in range(0, len(bf), linewidth))
